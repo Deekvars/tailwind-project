dd9a5f50a8a8a056e895ffb0737b0eebec04f046
deekvar <deeksha@bitqit.com>
2022-08-21 22:57:03 +0530

    first tailwind commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <div id="app">
-    <nav>
+<!--    <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-    <router-view/>
+    <router-view/>-->
+
+    <header>
+      <div class="search-box mt-6">
+        <input class="w-96 p-0.5 pl-4 pr-4 bg-white shadow rounded" name="q" autocomplete="off" placeholder="Search">
+      </div>
+    </header>
+<!--    <div class="container mx-auto">-->
+      <div class="product-card grid grid-cols-4 gap-1 container mx-20 max-w-fit mt-5">
+
+        <product-card></product-card>
+        <product-card></product-card>
+        <product-card></product-card>
+        <product-card></product-card>
+      </div>
+<!--    </div>-->
+    <footer></footer>
   </div>
 </template>
 
@@ -30,3 +46,9 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
+<script>
+import ProductCard from "@/components/Product-cards";
+export default {
+  components: {ProductCard}
+}
+</script>
---
+++ src/components/Product-cards.vue
@@ -0,0 +1,10 @@
+<template>
+<!--  <div class="container mx-auto">-->
+    <div id="product-card" class="border border-black">
+
+      <img class="" src="//cdn.shopify.com/s/files/1/0579/7924/0580/products/7.5-inch-set-of-5-multi-1_1.png">
+      <span class="prod-title">Ugaoo Krish Self Watering Planter -7.5 inch (Set of 5)</span>
+      <span class="prod-price">Rs. 125.00</span>
+    </div>
+<!--  </div>-->
+</template>
