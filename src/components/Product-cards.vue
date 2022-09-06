fb2ff263569679e3b7eba804d18837fc2ded451e
deekvar <deeksha@bitqit.com>
2022-09-06 22:17:01 +0530

    so called clear code

--- src/components/Product-cards.vue
+++ src/components/Product-cards.vue
@@ -1,12 +1,11 @@
 <template>
-<!--  <div class="container mx-auto">-->
     <div id="product-card" class="border border-black">
 
-      <img class="" :src="item.image" :alt="item.title">
-      <span class="prod-title">{{ item.title }})</span>
-      <span class="prod-price">{{ currency }} {{ item.price }}</span>
+      <img :src="item.image" :alt="item.title">
+      <span>{{ item.title }})</span>
+      <span>{{ currency }} {{ item.discounted_price }}</span>
+      <span v-if="item.price > item.discounted_price">{{ currency }} {{ item.price }}</span>
     </div>
-<!--  </div>-->
 </template>
 
 <script>
@@ -27,27 +26,5 @@ export default {
   }
 }
 </script>
-<!--<script>
 
-import projectData from "../globalVariables/config.json";
-
-export default {
-  name: 'Product-cards',
- data() {
-   return {
-     resultData : projectData.data,
-     currency: 'Rs.'
-   }
- },
-  methods() {
-    return{
-      priceFormatter: function(price) {
-        return price.toString().concat('.00');
-      }
-    }
-
-  }
-}
-
-</script>-->
 
